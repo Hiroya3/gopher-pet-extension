6b30aaf65b0bd4254a843a809513804fa30d919b
terry <s.3.h.st.sp211@gmail.com>
2025-11-23 23:18:36 +0900

    Add new media assets for Gopher Pet webview, including background and animation GIFs, and update webview HTML structure to incorporate background image.

--- src/gopherViewProvider.ts
+++ src/gopherViewProvider.ts
@@ -16,22 +16,53 @@ export class GopherViewProvider implements vscode.WebviewViewProvider {
             localResourceRoots: [vscode.Uri.joinPath(this._context.extensionUri, 'media')],
         };
 
-        const gopherUri = webviewView.webview.asWebviewUri(vscode.Uri.joinPath(this._context.extensionUri, 'media', 'gopher.PNG'));
-        webviewView.webview.html = this.getHtmlForWebview(webviewView.webview, gopherUri);
+        const gopherUri = webviewView.webview.asWebviewUri(vscode.Uri.joinPath(this._context.extensionUri, 'media', 'gopher_running.GIF'));
+        const backgroundUri = webviewView.webview.asWebviewUri(vscode.Uri.joinPath(this._context.extensionUri, 'media', 'background_glass.png'));
+        webviewView.webview.html = this.getHtmlForWebview(webviewView.webview, gopherUri, backgroundUri);
     }
 
-    private getHtmlForWebview(webview: vscode.Webview, imageUri: vscode.Uri): string {
+    private getHtmlForWebview(webview: vscode.Webview, imageUri: vscode.Uri, backgroundUri: vscode.Uri): string {
         return `
             <!DOCTYPE html>
             <html lang="en">
             <head>
                 <meta charset="UTF-8">
-                <meta http-equiv="Content-Security-Policy" content="default-src 'none'; img-src ${webview.cspSource};">
+                <meta http-equiv="Content-Security-Policy" content="
+                    default-src 'none';
+                    img-src ${webview.cspSource};
+                    style-src 'unsafe-inline';
+                    script-src 'unsafe-inline';
+                ">
             </head>
+    
             <body>
-                <img src="${imageUri}" alt="Gopher Pet">
+                <img id="gopher" src="${imageUri}" alt="Gopher Pet">
             </body>
+    
+            <style>
+                body {
+                    margin: 0;
+                    padding: 0;
+                    height: 100vh;
+                    display: flex;
+                    align-items: flex-end;
+                    justify-content: center;
+                    overflow: hidden;
+    
+                    background-image: url('${backgroundUri}');
+                    background-size: cover;
+                    background-position: center;
+                    background-repeat: no-repeat;
+                }
+    
+                #gopher {
+                    width: 60px;
+                    z-index: 2;
+                }
+            </style>
+    
             </html>
         `;
     }
+    
 }

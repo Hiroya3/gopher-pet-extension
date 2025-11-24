83f155d3dc37752c97acdad64bdf2e314be5ffa4
terry <s.3.h.st.sp211@gmail.com>
2025-11-24 21:54:16 +0900

    Remove Gopher Pet command from package.json and clean up related activation code in extension.ts

--- src/extension.ts
+++ src/extension.ts
@@ -6,7 +6,6 @@ import { GopherViewProvider } from './gopherViewProvider';
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
 	const provider = new GopherViewProvider(context);
 	context.subscriptions.push(
 		vscode.window.registerWebviewViewProvider(
@@ -14,61 +13,7 @@ export function activate(context: vscode.ExtensionContext) {
 			provider,
 		)
 	)
-
-	let currentPanel: vscode.WebviewPanel | undefined = undefined;
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const showGopherPet = vscode.commands.registerCommand(
-		'gopher-pet.showGopherPet', 
-		() => {
-
-			// the column num (one: left, two: right....) in which user shows the editor
-			const columnToShowIn = vscode.window.activeTextEditor ? vscode.window.activeTextEditor.viewColumn : undefined;
-
-			if (currentPanel){
-				currentPanel.reveal(columnToShowIn);
-			} else {
-				currentPanel = vscode.window.createWebviewPanel(
-					'gopherView',
-					'Gopher Pet',
-					columnToShowIn || vscode.ViewColumn.One,
-					{
-						enableScripts: true,
-						// localResourceRoots: [vscode.Uri.joinPath(context.extensionUri, 'media')]
-					}
-				);
-				currentPanel.webview.html = getWebviewContent(currentPanel.webview,context);
-
-				// dispose the panel when the user closes it
-				currentPanel.onDidDispose(() => {
-					currentPanel = undefined;
-				}, null, context.subscriptions);
-			}
-		}
-	);
-
-	context.subscriptions.push(showGopherPet);
 }
 
 // This method is called when your extension is deactivated
 export function deactivate() {}
-
-function getWebviewContent(webview: vscode.Webview, context: vscode.ExtensionContext): string {
-	// const gopherImage = webview.asWebviewUri(vscode.Uri.joinPath(context.extensionUri, 'media', 'gopher.png'));
-
-	return `
-		<!DOCTYPE html>
-		<html lang="en">
-		<head>
-			<meta charset="UTF-8">
-			<meta name="viewport" content="width=device-width, initial-scale=1.0">
-			<title>Gopher Pet</title>
-		</head>
-		<body>
-			<img src="https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif" alt="Gopher Pet">
-		</body>
-		</html>
-	`;
-}
